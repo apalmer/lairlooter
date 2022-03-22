456d1885aee1032cabc29b4e2462d3bb9dbd8f8a
Adigun Palmer <adigun.palmer@hotmail.com>
2022-03-22 00:29:08 -0400

    Added lairlooter home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+function handleNavigation(direction){
+
+}
+
 function App() {
+
+  let name = "diggy";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div class="wrapper">
+        <nav class="nav-wrapper">
+          <ul class="ul-wrapper">
+            <li class="north"><a href="#/North" onClick={() => handleNavigation("North")}>North</a></li>
+            <li class="south"><a href="#/South" onClick={() => handleNavigation("South")}>South</a></li>
+            <li class="west"><a href="#/West" onClick={() => handleNavigation("West")}>West</a></li>
+            <li class="east"><a href="#/East" onClick={() => handleNavigation("East")}>East</a></li>
+          </ul>
+        </nav>
+        <div class="viewer">
+          <h1>Hi {name}!</h1>
+        </div>
+      </div>
+    </main>
   );
 }
 
